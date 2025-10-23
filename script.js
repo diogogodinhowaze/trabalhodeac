67337bd9f06db1dcbba8745f5639ca0025aaf2e8
diogogodinhowaze <diogoalcobiagodinho@gmail.com>
2025-10-23 13:15:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Botão para abrir o relatório na mesma aba
+document.getElementById("relatorioBtn").addEventListener("click", function() {
+    window.location.href = "relatorio.html";
+});
+
+// Botão para abrir a página de pseudocódigo na mesma aba
+document.getElementById("pseudocodigoBtn").addEventListener("click", function() {
+    window.location.href = "pseudocodigo.html";
+});
